dd65f59c5b7beb97cbf33d91c4e76d258405fb1a
drewsef-916 <drewm541@gmail.com>
2019-06-30 10:58:30 -0700

    more base url fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 module.exports = {
   env: {
-    baseUrl: process.env.BASE_URL || 'https://whats-for-dinner.netlify.com',
+    baseUrl: process.env.BASE_URL || 'http://localhost:3000',
   },
 
   mode: 'universal',
