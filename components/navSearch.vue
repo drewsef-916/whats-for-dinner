c992901a0a757b918294ae9a638404444e5aeea2
Drew Moore <drewmoore@US-SAC-AL-H2103.local>
2019-07-11 17:08:52 -0700

    figure out nuxt links in dropdown list

--- components/navSearch.vue
+++ components/navSearch.vue
@@ -7,6 +7,7 @@
     <aside class="searchbox">
       <input type="text" placeholder="find a recipe..." @keyup="findMatches">
       <fa icon="times-circle" class="icon close" @click="buttonClose"/>
+      <ul class="recipe-matches"></ul>
     </aside>
   </div>
 </template>
@@ -24,10 +25,21 @@ export default {
     },
     findMatches: function() {
       let matchWord = this.$el.lastChild.firstChild.value;
-      const recipes = this.$props.recipeList;
-      const regex = new RegExp(matchWord, 'gi');
-      
-    }
+      const recipeList = this.$props.recipeList;
+      const suggestions = this.$el.lastChild.lastChild;
+      let filteredList = recipeList.filter(recipe => {
+        const regex = new RegExp(matchWord, 'gi');
+        return recipe.name.match(regex)
+      })
+      const html = filteredList.map(recipe => {
+        return `
+        <nuxt-link :to="{name: 'recipes-id', params: {id: ${recipe.id}}}">
+            <li>${recipe.name}</li>
+        </nuxt-link>
+        `
+      }).join('')
+      suggestions.innerHTML = html;
+    },
   }
 }
 </script>
@@ -66,5 +78,16 @@ export default {
     width: 50%;
     margin: 0 auto;
     color: slategrey;
+  }
+  .recipe-matches {
+    padding-left: 10px;
+    margin-top: 30px;
+  }
+  .recipe-matches li {
+    font-size: 1.5rem;
+    padding: 10px 0;
+    color: #494747;
+    background-color: rgba(255, 255, 255, 0.9);
+    border-bottom: 1px solid black;
   }
 </style>
