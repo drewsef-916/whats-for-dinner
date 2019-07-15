f246e74a963496d62e0fb9788f6a9ccc7171b0e6
Drew Moore <drewmoore@US-SAC-AL-H2103.local>
2019-07-15 09:12:19 -0700

    axios stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,7 +72,7 @@ module.exports = {
   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-    baseUrl: process.env.API_URL || 'http://localhost:3000'
+    baseURL: process.env.API_URL || 'http://localhost:3000'
   },
 
   /*
