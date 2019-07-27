6ddffb524086664453cc18050e3933dbec460d21
Drew Moore <30642898+drewsef-916@users.noreply.github.com>
2019-07-27 09:56:11 -0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -91,7 +91,7 @@ module.exports = {
   },
   generate: {
     routes: function () {
-      return axios.get(`/api/recipes`)
+      return axios.get(`${axios.baseURL}/api/recipes`)
       .then((res) => {
         return res.data.map((recipe) => {
           return {
