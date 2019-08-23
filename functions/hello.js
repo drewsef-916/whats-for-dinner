4c16c5ee632faca04d7251b48c93110a12863c18
Drew Moore <drewmoore@ip-172-27-222-160.us-east-2.compute.internal>
2019-08-23 16:27:50 -0700

    added functions folder

---
+++ functions/hello.js
@@ -0,0 +1,11 @@
+require("dotenv").config;
+
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Hellllooooo",
+      event
+    })
+  }
+}
