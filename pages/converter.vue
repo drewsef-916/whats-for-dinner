b34f2ec53dd964a4119bc6955bba0c399e565d7a
Drew Moore <drewmoore@ip-172-27-222-249.us-east-2.compute.internal>
2019-09-19 16:25:37 -0700

    trying to get fractions to work

--- pages/converter.vue
+++ pages/converter.vue
@@ -3,7 +3,7 @@
         <main class="converter-wrapper">
             <h3>Measurement Converter</h3>
             <div>
-              <input type="text" ref="unitNumber" placeholder="Quantity">
+              <input type="number" step="0.25" ref="unitNumber">
               <select ref="fromUnit">
                 <option value="cups">Cups</option>
                 <option value="quarts">Quarts</option>
@@ -18,57 +18,85 @@
                 <option value="tablespoons">Tablespoons</option>
               </select>
               <button @click="convertUnits($refs.fromUnit.value, $refs.toUnit.value)">Convert Units</button>
-              <h6 class="output" ref="output">{{this.conversion}}</h6>
+              <h6 class="output" ref="output"></h6>
             </div>
         </main>
     </div>
 </template>
 
 <script>
-// 1 cup = 0.25 quarts, 8 ounces, 16 tablespoons, 48 teaspoons
-const conversionConstants = [
-  {"cups": [
-    {"quarts": 0.25},
-    {"teaspoons": 48},
-    {"tablespoons": 16}
-  ]},
-  {"quarts": [
-    {"cups": 4},
-    {"tablespoons": 64},
-    {"teaspoons": 192}
-  ]},
-  {"tablespoons": [
-    {"cups": 0.0625},
-    {"quarts": .015625},
-    {"teaspoons": 3}
-  ]},
-  {"teaspoons": [
-    {"cups": .025},
-    {"quarts": 0.00520833}, //192 teaspoons in a quart
-    {"tablespoons": 0.333} //64 tablespoons in a quart
-  ]},
-]
-const roundingRules = [
-  {.250: "1/4"},
-  {.750: "3/4"},
-  {.333: "1/3"},
-  {.667: "2/3"},
-  {.125: "1/8"},
-  {.375: "3/8"},
-  {.625: "5/8"},
-  {.875: "7/8"},
-  {.500: "1/2"},
-]
-export default {
-  data: function() {
-    conversion: 0
+const conversionConstants = {
+  "cups": {
+    "quarts": 0.25,
+    "teaspoons": 48,
+    "tablespoons": 16
   },
+  "quarts": {
+    "cups": 4,
+    "tablespoons": 64,
+    "teaspoons": 192
+  },
+  "tablespoons": {
+    "cups": 0.0625,
+    "quarts": .015625,
+    "teaspoons": 3
+  },
+  "teaspoons": {
+    "cups": .025,
+    "quarts": 0.00520833, //192 teaspoons in a quart
+    "tablespoons": 0.333 //64 tablespoons in a quart
+  },
+}
+const roundingRules = {
+  0.125: "1/8",
+  0.250: "1/4",
+  0.333: "1/3",
+  0.375: "3/8",
+  0.500: "1/2",
+  0.625: "5/8",
+  0.666: "2/3",
+  0.750: "3/4",
+  0.875: "7/8",
+}
+export default {
   methods: {
+    numberRounder(num) {
+      console.log(num)
+      const keys = Object.keys(roundingRules)
+      let returnFraction = 0;
+      keys.forEach((value, index) => {
+        if (num === value) {
+          returnFraction = roundingRules[value]
+          return
+        } else if (num < 0.0625) {
+          returnFraction = 0
+          return
+        } else if (num > 0.9375) {
+          returnFraction = 1
+          return
+        } else if (num < value){
+          const halfwayPoint = (keys[index] - keys[index - 1]) / 2
+          num >= halfwayPoint ? returnFraction = roundingRules[keys[index]] :
+          returnFraction = roundingRules[keys[index - 1]]
+        }
+      })
+      return returnFraction
+    },
     convertUnits(from, to) {
-      const conversionValue = ((conversionConstants.from * $refs.unitNumber) * conversionConstants.to).toFixed(3)
-      const remainder = conversionValue % 1
-      console.log(remainder)
-      if (remainder == "000") this.conversion = conversionValue
+      const conversionNumber = this.$refs.unitNumber.value
+      const conversionValue = (conversionNumber * conversionConstants[from][to]).toFixed(3)
+      const remainder = (conversionValue % 1).toFixed(3)
+      // if (remainder < 0.0625) {
+      //   this.$refs.output.innerHTML = `${conversionNumber} ${from} equals ${conversionValue} ${to}`
+      // } else {
+      //   const fraction = this.numberRounder(remainder)
+      //   if (fraction === 1) {
+      //     const roundUp = conversionValue++
+      //     this.$refs.output.innerHTML = `${conversionNumber} ${from} equals ${roundUp} ${to}`
+      //   } else {
+      //     this.$refs.output.innerHTML = `${conversionNumber} ${from} equals ${conversionValue} and ${fraction} ${to}`
+      //   }
+      // }
     }
   }
 }
